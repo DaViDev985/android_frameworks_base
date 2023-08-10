0ee5607eea201479ae9a20d1b1f9322d6791a9de
jhenrique09 <jhsv09@gmail.com>
2023-08-10 14:22:06 +0000

    Implement DeviceConfigUtils and service
    - Allow overriding device configs while using GMS as device configurator package
    
    Change-Id: Ie7327610feca7a390915511088dfef831c946853
    
    DeviceConfigUtils: Make debug optional
    
    Change-Id: I65ffac8fd0a9971297b8d721d860face571a2d44
    (cherry picked from commit 9f54af5392d7ca70e1e495fc97560538459844e7)
    
    DeviceConfig: Change setProperties behavior
    
    Change-Id: I7cf1574f423c7362e6a7c9d8a002e4b20e5e7f87
    (cherry picked from commit f94a2c97e7bf53ee6681183f2394878369e99470)
    
    * Adapt to Android 14
    
    Change-Id: I6ece899c13f12ee299ba24b02f2b4ab96f3d92fb
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    [nurkeinneid: Fix string comparison for namespace and property checks]
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -169,6 +169,7 @@ import com.android.server.cpu.CpuMonitorService;
 import com.android.server.crashrecovery.CrashRecoveryAdaptor;
 import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
+import com.android.server.derpfest.CustomDeviceConfigService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.DisplayManagerService;
@@ -2822,6 +2823,11 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(BackgroundInstallControlService.class);
                 t.traceEnd();
             }
+
+            // CustomDeviceConfigService
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
+            t.traceEnd();
 
             // AttestationService
             t.traceBegin("AttestationService");
